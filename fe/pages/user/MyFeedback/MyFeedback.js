7b44a40eeb4e75c25eac28cf60f7551bce126a06
DKhiem017 <100778307+DKhiem017@users.noreply.github.com>
2024-05-27 22:17:30 +0700

    Logic Feedback

--- fe/pages/user/MyFeedback/MyFeedback.js
+++ fe/pages/user/MyFeedback/MyFeedback.js
@@ -1,9 +1,247 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  FlatList,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Pagetitle from "../../../components/pagetitle";
+import { FontAwesome } from "@expo/vector-icons";
+import { useState } from "react";
+import "react-native-gesture-handler";
+import { TextInput } from "react-native-gesture-handler";
 
 const plant_img = require("../../../../assets/images/Monstera_tran.png");
+
+const plantData = [
+  { id: 1, name: "Monstera", price: 30.55 },
+  { id: 2, name: "Fiddle Leaf Fig", price: 25.99 },
+];
+
+const plantReviewed = [
+  {
+    id: 1,
+    name: "Monstera",
+    rating: "4.5",
+    date: "05/04/2024",
+    comment: "Hàng đẹp, chất lượng",
+  },
+  {
+    id: 2,
+    name: "Fiddle Leaf Fig",
+    rating: "4",
+    date: "05/04/2024",
+    comment: "Hàng đẹp, chất lượng",
+  },
+];
+
+const MyFeedback = ({ navigation }) => {
+  //rating function
+  const [defaultRating, setdefaultRating] = useState(0);
+  const [maxRating, setmaxRating] = useState([1, 2, 3, 4, 5]);
+  //show Feedback Form
+  const [showFeedback, setshowFeedback] = useState(false);
+  const ItemReviewed = ({ name, rating, date, comment }) => (
+    <View style={styles.itemBackground}>
+      <View style={{ flexDirection: "row" }}>
+        <View style={styles.imgBorder}>
+          <Image source={plant_img} style={styles.backgroundImg}></Image>
+        </View>
+        <View style={{ justifyContent: "space-between", marginLeft: 10 }}>
+          <Text style={{ color: "#000", fontWeight: 600 }}>{name}</Text>
+          <View style={{ flexDirection: "row", gap: 2, alignItems: "center" }}>
+            <FontAwesome name="star" size={12} color="#498553" />
+            <Text style={{ color: "#498553", marginRight: 5, fontSize: 12 }}>
+              {rating}
+            </Text>
+            <Text style={{ fontSize: 12 }}>{date}</Text>
+          </View>
+          <Text style={{ color: "#498553", fontWeight: 400, fontSize:11 }}>{comment}</Text>
+        </View>
+      </View>
+    </View>
+  );
+
+  const ItemUnreviewed = ({ name, price }) => (
+    <View style={styles.itemBackground}>
+      <View style={{ flexDirection: "row" }}>
+        <View style={styles.imgBorder}>
+          <Image source={plant_img} style={styles.backgroundImg}></Image>
+        </View>
+        <View style={{ justifyContent: "space-between", marginLeft: 10 }}>
+          <Text style={{ color: "#000", fontWeight: 600 }}>{name}</Text>
+          <Text style={{ color: "#498553", fontWeight: 600 }}>${price}</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.feedBackBut}
+          onPress={() => setshowFeedback(true)}
+        >
+          <Text style={{ color: "#fff", fontWeight: 400, fontSize: 11 }}>
+            Feedback
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+  //renderItem
+  const renderItemUnreviewed = ({ item }) => (
+    <ItemUnreviewed name={item.name} price={item.price} />
+  );
+
+  const renderItemReviewed = ({ item }) => (
+    <ItemReviewed
+      name={item.name}
+      comment={item.comment}
+      rating={item.rating}
+      date={item.date}
+    />
+  );
+
+  //set View
+  const [reviewed, setReviewed] = useState(false);
+
+  //chuyển đổi view giữa unreviewed và reviewed
+  const handleUnreviewed = () => {
+    if (reviewed == true) {
+      setReviewed(false);
+    }
+  };
+
+  const handleReviewed = () => {
+    if (reviewed == false) {
+      setReviewed(true);
+    }
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#f5f5f5" }}>
+      <View>
+        <StatusBar></StatusBar>
+        <Pagetitle title={"My Feedback"} navigation={navigation}></Pagetitle>
+        <View
+          style={{ flexDirection: "row", marginTop: 10, paddingHorizontal: 10 }}
+        >
+          {/* Unreview */}
+          <TouchableOpacity
+            style={styles.tagContainer}
+            onPress={handleUnreviewed}
+          >
+            <Text
+              style={[
+                styles.tagTitle,
+                { color: reviewed === true ? "#6F6A61" : "#498553" },
+              ]}
+            >
+              Unreviewed
+            </Text>
+            {!reviewed && <View style={styles.underline}></View>}
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.tagContainer}
+            onPress={handleReviewed}
+          >
+            <Text
+              style={[
+                styles.tagTitle,
+                { color: reviewed === false ? "#6F6A61" : "#498553" },
+              ]}
+            >
+              Reviewed
+            </Text>
+            {reviewed && <View style={styles.underline}></View>}
+          </TouchableOpacity>
+        </View>
+        {/* list item */}
+        {!reviewed && (
+          <View
+            style={{
+              paddingTop: 5,
+              width: "100%",
+              marginTop: 10,
+              paddingHorizontal: 15,
+            }}
+          >
+            <FlatList
+              data={plantData}
+              renderItem={renderItemUnreviewed}
+              keyExtractor={(item) => item.id}
+              style={{ width: "100%" }}
+            ></FlatList>
+          </View>
+        )}
+        {reviewed && (
+          <View
+            style={{
+              paddingTop: 5,
+              width: "100%",
+              marginTop: 10,
+              paddingHorizontal: 15,
+            }}
+          >
+            <FlatList
+              data={plantReviewed}
+              renderItem={renderItemReviewed}
+              keyExtractor={(item) => item.id}
+              style={{ width: "100%" }}
+            ></FlatList>
+          </View>
+        )}
+      </View>
+      {showFeedback && (
+        <View style={styles.Formcontainer}>
+          <View style={styles.feedbackForm}>
+            <Text style={{ fontSize: 16, color: "#498553", fontWeight: "600" }}>
+              {" "}
+              Review
+            </Text>
+            <View style={styles.customRatingbar}>
+              {maxRating.map((item, key) => {
+                return (
+                  <TouchableOpacity
+                    activeOpacity={0.7}
+                    key={item}
+                    onPress={() => setdefaultRating(item)}
+                  >
+                    <FontAwesome
+                      name={item <= defaultRating ? "star" : "star-o"}
+                      size={30}
+                      color="yellow"
+                    />
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+            <View>
+              <TextInput
+                multiline
+                numberOfLines={4}
+                style={styles.inputLabel}
+              ></TextInput>
+            </View>
+            <View style={{ flexDirection: "row", gap: 10 }}>
+              <TouchableOpacity
+                style={styles.cancelButtonStyle}
+                onPress={() => setshowFeedback(false)}
+              >
+                <Text style={{ fontSize: 12, color: "#fff", fontWeight: 500 }}>
+                  Cancel
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.saveButtonStyle}>
+                <Text style={{ fontSize: 12, color: "#fff", fontWeight: 500 }}>
+                  Save
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      )}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   itemBackground: {
@@ -19,6 +257,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     paddingHorizontal: 5,
     paddingVertical: 7,
+    marginBottom: 15,
   },
   backgroundImg: {
     position: "absolute",
@@ -26,130 +265,84 @@ const styles = StyleSheet.create({
     width: "100%",
     resizeMode: "contain",
   },
+  tagContainer: {
+    width: "50%",
+    alignItems: "center",
+  },
+  underline: {
+    backgroundColor: "#498553",
+    height: 2,
+    width: "100%",
+    marginTop: 3,
+  },
+  tagTitle: {
+    fontWeight: 600,
+    fontSize: 16,
+  },
+  imgBorder: {
+    backgroundColor: "#DAF1D4",
+    height: 60,
+    width: 49,
+    borderRadius: 5,
+  },
+  feedBackBut: {
+    height: 20,
+    width: 90,
+    backgroundColor: "#498553",
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 0,
+    bottom: 0,
+  },
+  feedbackForm: {
+    width: 350,
+    height: 320,
+    borderWidth: 1,
+    borderColor: "#498553",
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    zIndex: 1,
+    alignItems: "center",
+    paddingTop: 10,
+    paddingBottom: 20,
+    justifyContent: "space-between",
+  },
+  Formcontainer: {
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  customRatingbar: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  inputLabel: {
+    width: 320,
+    height: 185,
+    borderWidth: 1,
+    borderColor: "#498553",
+    borderRadius: 10,
+  },
+  cancelButtonStyle: {
+    height: 30,
+    width: 80,
+    borderRadius: 10,
+    backgroundColor: "#E71818",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  saveButtonStyle: {
+    height: 30,
+    width: 80,
+    borderRadius: 10,
+    backgroundColor: "#498553",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
-
-const MyFeedback = ({navigation}) => {
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#f5f5f5" }}>
-      <View>
-        <StatusBar></StatusBar>
-        <Pagetitle title={"My Feedback"} navigation={navigation}></Pagetitle>
-        <View
-          style={{ flexDirection: "row", marginTop: 10, paddingHorizontal: 10 }}
-        >
-          {/* Unreview */}
-          <TouchableOpacity style={{ width: "50%", alignItems: "center" }}>
-            <Text style={{ color: "#498553", fontWeight: 600, fontSize: 16 }}>
-              Unreviewed
-            </Text>
-            <View
-              style={{
-                backgroundColor: "#498553",
-                height: 2,
-                width: "100%",
-                marginTop: 3,
-              }}
-            ></View>
-          </TouchableOpacity>
-          <TouchableOpacity style={{ width: "50%", alignItems: "center" }}>
-            <Text style={{ color: "#6F6A61", fontWeight: 600, fontSize: 16 }}>
-              Reviewed
-            </Text>
-            {/* <View
-              style={{
-                backgroundColor: "#498553",
-                height: 2,
-                width: "100%",
-                marginTop: 3,
-              }}
-            ></View> */}
-          </TouchableOpacity>
-        </View>
-        {/* list item */}
-        <View style={{ paddingHorizontal: 15, marginTop: 20, gap: 15 }}>
-          <View style={styles.itemBackground}>
-            <View style={{ flexDirection: "row" }}>
-              {/* ảnh sp */}
-              <View
-                style={{
-                  backgroundColor: "#DAF1D4",
-                  height: 60,
-                  width: 49,
-                  borderRadius: 5,
-                }}
-              >
-                <Image source={plant_img} style={styles.backgroundImg}></Image>
-              </View>
-              <View style={{ justifyContent: "space-between", marginLeft: 10 }}>
-                <Text style={{ color: "#000", fontWeight: 600 }}>Monstera</Text>
-                <Text style={{ color: "#498553", fontWeight: 600 }}>
-                  $30.55
-                </Text>
-              </View>
-              {/* feedbackbutton */}
-              <TouchableOpacity
-                style={{
-                  height: 20,
-                  width: 90,
-                  backgroundColor: "#498553",
-                  borderRadius: 5,
-                  justifyContent: "center",
-                  alignItems: "center",
-                  position: "absolute",
-                  right: 0,
-                  bottom: 0,
-                }}
-              >
-                <Text style={{ color: "#fff", fontWeight: 400, fontSize: 11 }}>
-                  Feedback
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-          {/* item sp */}
-          <View style={styles.itemBackground}>
-            <View style={{ flexDirection: "row" }}>
-              {/* ảnh sp */}
-              <View
-                style={{
-                  backgroundColor: "#DAF1D4",
-                  height: 60,
-                  width: 49,
-                  borderRadius: 5,
-                }}
-              >
-                <Image source={plant_img} style={styles.backgroundImg}></Image>
-              </View>
-              <View style={{ justifyContent: "space-between", marginLeft: 10 }}>
-                <Text style={{ color: "#000", fontWeight: 600 }}>Monstera</Text>
-                <Text style={{ color: "#498553", fontWeight: 600 }}>
-                  $30.55
-                </Text>
-              </View>
-              {/* feedbackbutton */}
-              <TouchableOpacity
-                style={{
-                  height: 20,
-                  width: 90,
-                  backgroundColor: "#498553",
-                  borderRadius: 5,
-                  justifyContent: "center",
-                  alignItems: "center",
-                  position: "absolute",
-                  right: 0,
-                  bottom: 0,
-                }}
-              >
-                <Text style={{ color: "#fff", fontWeight: 400, fontSize: 11 }}>
-                  Feedback
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
-};
 
 export default MyFeedback;
